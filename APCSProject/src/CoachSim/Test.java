503dc6dc6e43d80efbc888fa92795844313e540d
ADLeonelli <leonelliad@gmail.com>
2016-05-05 13:13:47 -0700

    Created player class
    don't have eclipse on this computer so i did what I could

--- APCSProject/src/CoachSim/Test.java
+++ APCSProject/src/CoachSim/Test.java
@@ -2,9 +2,47 @@ package CoachSim;
 
 public class Test {
 
-	public static void main(String[] args) {
-		System.out.println("Hello Worl!");
-
+	private String name;
+	private int num;
+	
+	public Player(){	
 	}
+	
+	public Player(String n){
+		setName(n);
+		setNum(0);
+	}
+	
+	public Player(int number) {
+		setNum(number);
+		setName("Player");
+	}
+	
+	public Player(String n, int number) {
+		setNum(number);
+		setName(n);
+	}
+	
+	public void setName(String n){
+		name = n;
+	}
+	
+	public void setNum(int number){
+		num = number;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getNum(){
+		return num;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [name=" + name + ", num=" + num + "]";
+	}
+
 
 }
