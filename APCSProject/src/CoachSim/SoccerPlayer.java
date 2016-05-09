08b28f5f1ffe566160d4bff5e45d88d72a2b0879
maya3128 <maya@team3128.org>
2016-05-09 10:04:24 -0700

    Make Player Abstract
    Update Player to an abstract class and change SoccerPlayer accordingly.

--- APCSProject/src/CoachSim/SoccerPlayer.java
+++ APCSProject/src/CoachSim/SoccerPlayer.java
@@ -1,61 +1,31 @@
 package CoachSim;
 
-public class SoccerPlayer implements Player{
+public class SoccerPlayer extends Player{
 	private String position;
 	private String playerName;
 	private int number;
 	
 	public SoccerPlayer(){
+		super();
 	}
 	
 	public SoccerPlayer(String name){
-		setName(name);
-		setNum(0);
-		setPos("");
+		super(name);
 	}
 	
 	public SoccerPlayer(int number){
-		setNum(number);
-		setName("Unknown");
-		setPos("");
+
+		super(number);
 	}
 	
 	public SoccerPlayer(String name, int number){
-		setNum(number);
-		setName(name);
-		setPos("");
+		super(name, number);
 	}
 	
 	public SoccerPlayer(String name, int number, String pos){
-		setName(name);
-		setNum(number);
-		setPos(pos);
+		super(name, number, pos);
 	}
 	
-	public void setName(String name){
-		playerName = name;
-	}
-	
-	public void setNum(int num){
-		number=num;
-	}
-	
-	public void setPos(String pos){
-		position = pos;
-	}
-	
-	public String getName(){
-		return playerName;
-	}
-	
-	public int getNumber(){
-		return number;
-	}
-	
-	public String getPos(){
-		return position;
-	}
-
 	@Override
 	public String toString() {
 		return "Player Info::/nName::"+getName()+"/nNumber::"+getNumber()+"/nPosition::"+getPos();
