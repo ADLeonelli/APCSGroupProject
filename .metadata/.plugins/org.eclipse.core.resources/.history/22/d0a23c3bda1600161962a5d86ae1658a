025c1a0f2280af46e767d1f40ebeacef99f92816
arejto <alex.rejto51@gmail.com>
2016-05-10 11:09:09 -0700

    soccerStat again

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/22/d0a23c3bda1600161962a5d86ae1658a
@@ -0,0 +1,23 @@
+package CoachSim;
+
+public class SoccerStat {
+	
+	private String stat;
+
+	
+	public SoccerStat() {
+		this(" ");
+		
+	}
+
+		
+	public SoccerStat(String stat) {
+		super();
+		this.stat = stat;
+	}
+	
+	public String getStat(){
+		return stat;
+	}
+	
+}
--- APCSProject/src/CoachSim/SoccerStat.java
+++ APCSProject/src/CoachSim/SoccerStat.java
@@ -10,6 +10,54 @@ public class SoccerStat {
 		
 	}
 
+	public int isGoal(){
+		if(stat.equals("G"))
+			return 1;
+		
+		else 
+			return 0;
+	}
+	
+	public int isShot(){
+		if(stat.equals("S") || stat.equals("G"))
+			return 1;
+		else
+			return 0;
+	}
+	public int Assist(){
+		if(stat.equals("A"))
+			return 1;
+		else
+			return 0;
+	}
+	
+	public int isCorner(){
+		if(stat.equals("C"))
+			return 1;
+		else
+			return 0;
+	}
+	
+	public int isFoul(){
+		if(stat.equals("F"))
+			return 1;
+		else
+			return 0;
+	}
+	
+	public int isRed(){
+		if(stat.equals("RC"))
+			return 1;
+		else
+			return 0;
+	}
+	
+	public int isYellow(){
+		if(stat.equals("YC"))
+			return 1;
+		else
+			return 0;
+	}
 		
 	public SoccerStat(String stat) {
 		super();
@@ -18,6 +66,12 @@ public class SoccerStat {
 	
 	public String getStat(){
 		return stat;
+	}
+	
+	public String toString(){
+		
+		return getStat() + "\n";
+		
 	}
 	
 }
