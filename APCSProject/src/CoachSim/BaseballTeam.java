6c14fa610626b6864ec572e65aa4ca766ab49140
madisonbma <madisonbma@gmail.com>
2016-05-10 21:47:16 -0700

    trying to test some stuff, need alex to fix the specific players

--- APCSProject/src/CoachSim/BaseballTeam.java
+++ APCSProject/src/CoachSim/BaseballTeam.java
@@ -1,5 +1,9 @@
 package CoachSim;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
 public class BaseballTeam extends Team{
 	
 	public BaseballTeam(){
@@ -12,6 +16,13 @@ public class BaseballTeam extends Team{
 	
 	public void addBaseballPlayer(String name, int number, String pos){
 		super.addPlayer(new BaseballPlayer(name, number, pos));
+	}
+	
+	public void save(String fileName) throws FileNotFoundException {
+		BaseballTeam bt = new BaseballTeam();
+	    PrintWriter pw = new PrintWriter(new FileOutputStream("src/CoachSim/BaseballOutput"));
+	        pw.println(bt);
+	    pw.close();
 	}
 }
 
