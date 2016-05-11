799fe48fbe7773a14d937da702d2e0470a72b77d
ADLeonelli <leonelliad@gmail.com>
2016-05-11 10:50:38 -0700

    merge

--- APCSProject/src/CoachSim/SoccerTeam.java
+++ APCSProject/src/CoachSim/SoccerTeam.java
@@ -1,5 +1,7 @@
 package CoachSim;
 
+import java.io.*;
+
 public class SoccerTeam extends Team{
 	
 	public SoccerTeam(){
@@ -13,4 +15,12 @@ public class SoccerTeam extends Team{
 	public void addSoccerPlayer(String name, int number, String pos){
 		super.addPlayer(new SoccerPlayer(name, number, pos));
 	}
+	
+	public void save(String fileName) throws FileNotFoundException {
+		SoccerTeam st = new SoccerTeam();
+	    PrintWriter pw = new PrintWriter(new FileOutputStream("src/CoachSim/Output"));
+	        pw.println(st);
+	    pw.close();
+	}
+	
 }
